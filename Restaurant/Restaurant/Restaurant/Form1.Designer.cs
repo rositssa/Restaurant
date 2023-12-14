b686fb35f87e13055d4617dcff6d9e9e3b502564
rositssa <120709153+rositssa@users.noreply.github.com>
2023-12-14 22:21:44 +0200

    Add files via upload

---
+++ Restaurant/Restaurant/Restaurant/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Restaurant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblRestaurant = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.txbx1 = new System.Windows.Forms.TextBox();
+            this.rbtn3 = new System.Windows.Forms.RadioButton();
+            this.rbtn4 = new System.Windows.Forms.RadioButton();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRestaurant
+            // 
+            this.lblRestaurant.AutoSize = true;
+            this.lblRestaurant.BackColor = System.Drawing.Color.Transparent;
+            this.lblRestaurant.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblRestaurant.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblRestaurant.Location = new System.Drawing.Point(192, 9);
+            this.lblRestaurant.Name = "lblRestaurant";
+            this.lblRestaurant.Size = new System.Drawing.Size(278, 58);
+            this.lblRestaurant.TabIndex = 0;
+            this.lblRestaurant.Text = "Ресторант";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl1.Location = new System.Drawing.Point(12, 150);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(227, 25);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Каква да бъде масата?";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl2.Location = new System.Drawing.Point(12, 81);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(251, 25);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "За колко човека да бъде?";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl3.Location = new System.Drawing.Point(12, 211);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(172, 25);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "Как ще платите?";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl4.Location = new System.Drawing.Point(12, 276);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(141, 25);
+            this.lbl4.TabIndex = 4;
+            this.lbl4.Text = "Към менюто:";
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.BackColor = System.Drawing.Color.AliceBlue;
+            this.rbtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbtn1.Location = new System.Drawing.Point(284, 148);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(127, 29);
+            this.rbtn1.TabIndex = 5;
+            this.rbtn1.TabStop = true;
+            this.rbtn1.Text = "за пушачи";
+            this.rbtn1.UseVisualStyleBackColor = false;
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.BackColor = System.Drawing.Color.AliceBlue;
+            this.rbtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbtn2.Location = new System.Drawing.Point(452, 146);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(149, 29);
+            this.rbtn2.TabIndex = 6;
+            this.rbtn2.TabStop = true;
+            this.rbtn2.Text = "за непушачи";
+            this.rbtn2.UseVisualStyleBackColor = false;
+            // 
+            // txbx1
+            // 
+            this.txbx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txbx1.Location = new System.Drawing.Point(305, 80);
+            this.txbx1.Name = "txbx1";
+            this.txbx1.Size = new System.Drawing.Size(135, 26);
+            this.txbx1.TabIndex = 7;
+            // 
+            // rbtn3
+            // 
+            this.rbtn3.AutoSize = true;
+            this.rbtn3.BackColor = System.Drawing.Color.AliceBlue;
+            this.rbtn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbtn3.Location = new System.Drawing.Point(230, 211);
+            this.rbtn3.Name = "rbtn3";
+            this.rbtn3.Size = new System.Drawing.Size(92, 29);
+            this.rbtn3.TabIndex = 8;
+            this.rbtn3.TabStop = true;
+            this.rbtn3.Text = "в брой";
+            this.rbtn3.UseVisualStyleBackColor = false;
+            // 
+            // rbtn4
+            // 
+            this.rbtn4.AutoSize = true;
+            this.rbtn4.BackColor = System.Drawing.Color.AliceBlue;
+            this.rbtn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbtn4.Location = new System.Drawing.Point(394, 211);
+            this.rbtn4.Name = "rbtn4";
+            this.rbtn4.Size = new System.Drawing.Size(101, 29);
+            this.rbtn4.TabIndex = 9;
+            this.rbtn4.TabStop = true;
+            this.rbtn4.Text = "с карта";
+            this.rbtn4.UseVisualStyleBackColor = false;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMenu.Location = new System.Drawing.Point(199, 276);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(203, 36);
+            this.btnMenu.TabIndex = 10;
+            this.btnMenu.Text = "Цъкни тук";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(628, 349);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.rbtn4);
+            this.Controls.Add(this.rbtn3);
+            this.Controls.Add(this.txbx1);
+            this.Controls.Add(this.rbtn2);
+            this.Controls.Add(this.rbtn1);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lblRestaurant);
+            this.Name = "Form1";
+            this.Text = "Добре дошли!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRestaurant;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.TextBox txbx1;
+        private System.Windows.Forms.RadioButton rbtn3;
+        private System.Windows.Forms.RadioButton rbtn4;
+        private System.Windows.Forms.Button btnMenu;
+    }
+}
+
